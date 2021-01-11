e967ab0cc112fa681fa955bb2ec66b15fe12df05
kromate <akpananthony33@gmail.com>
2021-01-11 22:12:59 +0100

    homepage_completed_75%

--- src/components/Catelogue.vue
+++ src/components/Catelogue.vue
@@ -3,8 +3,13 @@
     <h1>Home</h1>
     <div class="catCon">
       <div v-for="(cat, index) in catelogue" :key="index" class="catItem">
-        <div>
+        <div class="item">
           <img :src="cat.img" alt="" class="custom" />
+          <div class="flex">
+            <img src="@/assets/icon/Heart.svg" alt="" class="icon" />
+            <img src="@/assets/icon/addCart.svg" alt="" class="icon" />
+            <img src="@/assets/icon/share.svg" alt="" class="icon" />
+          </div>
         </div>
         <p class="name">{{ cat.name }}</p>
       </div>
@@ -35,12 +40,31 @@ export default {
 </script>
 
 <style scoped>
+.flex {
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+}
+.item {
+  width: 133.33px;
+}
+.icon {
+  background-color: rgba(0, 0, 0, 0.537);
+  border-radius: 100%;
+  padding: 4px;
+  height: 22px;
+  position: relative;
+  top: -105px;
+  right: -5px;
+  cursor: pointer;
+  margin-bottom: 3px;
+}
 .name {
   text-shadow: 0px 1px 4px rgba(0, 0, 0, 0.315), 0px 2px 2px rgba(206, 199, 184, 0.235);
   font-size: 1.2rem;
-  text-align: center;
+  /* text-align: center; */
   font-weight: 600;
-  margin-top: 0.5rem;
+  margin-top: -1.5rem;
 }
 .catItem {
   margin: 12px 9px;

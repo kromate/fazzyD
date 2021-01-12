cd1fa4b72d8a1a4ab8eed9beeb406c26662e7890
kromate <akpananthony33@gmail.com>
2021-01-12 22:15:20 +0100

    cartPage 40%

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -6,6 +6,27 @@
       <p class="otherCol">UNIT PRICE</p>
       <p class="otherCol">SUBTOTAL</p>
     </div>
+
+    <div class="row">
+      <div class="firstCol bod flex align">
+        <img src="@/assets/gallery/black_hoodie.png" class="cartImg" />
+        <div class="cartDet">
+          <h1>Zylo Hoodie</h1>
+          <p>Color: black</p>
+          <p>Size: XL</p>
+          <div class=" flex opt">
+            <div class="flex align">
+              <img src="@/assets/icon/Heart.svg" class="icon" />
+              <p class="iconText">Add To Favourite</p>
+            </div>
+            <div class="flex align ml">
+              <img src="@/assets/icon/Delete.svg" class="icon" />
+              <p class="iconText">Remove</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,11 +37,57 @@ export default {
 </script>
 
 <style scoped>
+.ml {
+  margin-left: 12px;
+}
+
+.iconText {
+  font-size: 9px;
+  font-weight: 600;
+}
+.opt {
+  margin-top: 10px;
+}
+.icon {
+  height: 15px;
+}
+.cartDet > p {
+  font-size: 0.9rem;
+  font-weight: 500;
+  font-style: italic;
+}
+h1 {
+  font-size: 1.3rem;
+  margin-bottom: 5px;
+}
+.cartDet {
+  display: flex;
+  flex-direction: column;
+  color: #d79947;
+  margin-left: 10px;
+}
+.bod {
+  border-right: 1px solid #d79947;
+}
+.cartImg {
+  height: 70px;
+  border-radius: 4%;
+}
+.row {
+  background: #000;
+  width: 800px;
+  max-width: 90vw;
+  display: flex;
+  padding: 3px 9px;
+  height: 100px;
+  align-items: center;
+}
 .header {
   margin-bottom: 5px;
   display: flex;
   width: 800px;
   max-width: 90vw;
+  text-align: center;
 }
 .firstCol {
   flex-basis: 40%;
@@ -34,6 +101,7 @@ export default {
   flex-direction: column;
   align-items: center;
   padding: 0.5rem;
+  margin-top: 2rem;
 }
 .header > p {
   color: #d79947;

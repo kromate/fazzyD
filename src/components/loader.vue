2c69271c0472d18860c40c0c2b3b69a558e56333
kromate <akpananthony33@gmail.com>
2021-01-18 17:05:03 +0100

    loader start

---
+++ src/components/loader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="load"></div>
+</template>
+
+<script>
+export default {
+  name: "loader",
+  props: ["h", "w"],
+
+  mounted() {
+    document.getElementsByClassName("load").style.display = "block";
+    document.getElementsByClassName("load").style.height = "500px";
+    document.getElementsByClassName("load").style.width = "500px";
+  },
+};
+</script>
+
+<style></style>

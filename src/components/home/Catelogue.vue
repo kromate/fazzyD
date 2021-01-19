e9f294592c754d048eadb1b3a78638c79aa7fb43
kromate <akpananthony33@gmail.com>
2021-01-19 22:57:06 +0100

    removed loader

--- src/components/home/Catelogue.vue
+++ src/components/home/Catelogue.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import Loader from "@/components/Loader.vue";
+// import Loader from "@/components/Loader.vue";
 export default {
   components: { Loader },
   name: "homeCatelogue",

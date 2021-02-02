18feccb2cbb9ec55c68062dab54ffe1a72f912e5
kromate <akpananthony33@gmail.com>
2021-02-02 23:22:21 +0100

    lower body

--- src/views/Custom_order.vue
+++ src/views/Custom_order.vue
@@ -64,8 +64,28 @@
       <div class="lower">
         <label class="heading">Lower Body Measurement</label>
         <div class="inputBox">
-          <label for="phone">PHONE NUM</label>
-          <input type="number" id="phone" placeholder="Enter Size in CM" />
+          <label for="Waist">Waist</label>
+          <input type="text" id="Waist" placeholder="Enter Size in CM" />
+        </div>
+        <div class="inputBox">
+          <label for="Hip">Hip</label>
+          <input type="text" id="Hip" placeholder="Enter Size in CM" />
+        </div>
+        <div class="inputBox">
+          <label for="Thigh">Thigh</label>
+          <input type="text" id="Thigh" placeholder="Enter Size in CM" />
+        </div>
+        <div class="inputBox">
+          <label for="Knee">Knee</label>
+          <input type="text" id="Knee" placeholder="Enter Size in CM" />
+        </div>
+        <div class="inputBox">
+          <label for="Ankle">Ankle</label>
+          <input type="text" id="Ankle" placeholder="Enter Size in CM" />
+        </div>
+        <div class="inputBox">
+          <label for="Length">Length</label>
+          <input type="text" id="Length" placeholder="Enter Size in CM" />
         </div>
       </div>
     </form>

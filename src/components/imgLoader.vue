a0cafa7e1b3f555e94a344bbe8d03b7f0f72343e
kromate <akpananthony33@gmail.com>
2021-02-13 23:07:01 +0100

    updated loader

--- src/components/imgLoader.vue
+++ src/components/imgLoader.vue
@@ -14,6 +14,7 @@ export default {
 
 <style scoped>
 .pulsate {
+  max-width: 306px !important;
   background: linear-gradient(30deg, #6a3f05, #a2691e, #804b0a, #a56417);
   background-size: 400% 400%;
   -webkit-animation: Gradient-04a0d67a 2.25s ease infinite;

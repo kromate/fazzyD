f2758847880bcecd07a1c9d215b877ccff575335
kromate <akpananthony33@gmail.com>
2021-03-09 22:32:53 +0100

    .env updates complete

--- src/main.js
+++ src/main.js
@@ -11,16 +11,14 @@ import store from './store'
 import './theme/main.css'
 
 const firebaseConfig = {
-    // apiKey: process.env.VUE_APP_API_KEY,
-    // authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
     // databaseURL: process.env.VUE_APP_DATABASE_URL,
-    // projectId: process.env.VUE_APP_PROJECT_ID,
-    // storageBucket : process.env.VUE_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.VUE_APP_MESSAGE_ID,
-    // appId: process.env.VUE_APP_APP_ID,
-    // measurementId: process.env.VUE_APP_MEASURE_ID
-
-  
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket : process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGE_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASURE_ID
   };
   
   firebase.initializeApp(firebaseConfig);
